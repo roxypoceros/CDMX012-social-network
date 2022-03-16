7a9962f545aa2dc3f1e162225b9ac18c12ef5cb0
roxanaggr <roxanaggr@gmail.com>
2022-03-16 00:04:54 -0600

    algo de estilos

--- src/componets/Login.js
+++ src/componets/Login.js
@@ -1,14 +1,14 @@
 import { onNavigate } from "../main.js";
 
-export const Register = () => {
-  const RegisterDiv = document.createElement('div');
+export const Login = () => {
+  const LoginDiv = document.createElement('div');
   const nodoh2 = document.createElement('h2');
   const buttonHome = document.createElement('button');
   buttonHome.textContent = 'Regresa a Home';
   buttonHome.addEventListener('click', () => {
     onNavigate('/');
   });
-  nodoh2.textContent = 'Ingresa tu correo';
-  RegisterDiv.append(nodoh2, buttonHome);
-  return RegisterDiv;
+  nodoh2.textContent = 'Inicia Sesión';
+  LoginDiv.append(nodoh2, buttonHome);
+  return LoginDiv;
 };
