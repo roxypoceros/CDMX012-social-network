23885713dadd4311d041e1215f97913481327877
roxanaggr <roxanaggr@gmail.com>
2022-03-24 17:37:59 -0600

    se agrego la autenticacion con gmail y login con cuenta de correo

--- src/componets/Login.js
+++ src/componets/Login.js
@@ -1,4 +1,7 @@
 import { onNavigate } from "../main.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/9.6.9/firebase-auth.js";
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.9/firebase-auth.js";
+
 export const Login = () => {
   const logoDivSmall = document.createElement('img');
   logoDivSmall.classList.add('logoDivSmall');
@@ -6,26 +9,89 @@ export const Login = () => {
   const loginDiv = document.createElement('div');
   loginDiv.setAttribute('id', 'loginDiv');
   const nodoh2 = document.createElement('h2');
-  const inputUser = document.createElement('input');
-  const labelUserName = document.createElement('h3');
-  labelUserName.textContent = 'USUARIO';
+  //const inputUser = document.createElement('input');
+  //const labelUserName = document.createElement('h3');
+  //labelUserName.textContent = 'USUARIO';
+
+  const labelEmail = document.createElement('h3');
+  labelEmail.textContent = 'EMAIL';
+  const inputEmail = document.createElement('input');
+  inputEmail.setAttribute('id', 'mailregister');
+
   const inputPassword = document.createElement('input');
   const labelPassword = document.createElement('h3');
   labelPassword.textContent = 'CONTRASEÑA';
+
   const buttonHome = document.createElement('button');
-  const buttonLogin = document.createElement('button');
   buttonHome.textContent = 'Regresa a Home';
-  inputPassword.setAttribute('id', 'password');
   buttonHome.setAttribute('id', 'homeButton');
-  buttonLogin.textContent = 'Ingresa';
+
+  inputPassword.setAttribute('id', 'password');
   inputPassword.setAttribute('type', 'password');
-  inputUser.placeholder = 'Ingresa tu Usuario';
+
+  const buttonLogin = document.createElement('button');
+  buttonLogin.textContent = 'Ingresa';
+
+  const buttonGmail = document.createElement('button')
+  buttonGmail.classList.add('buttonGmail')
+  buttonGmail.innerHTML = '<i class="fa-brands fa-google"></i>INICIA SESIÓN CON GOOGLE';
+  buttonGmail.setAttribute('id', 'btnGmail')
+  //buttonGmail.textContent = 'INICIA SESIÓN CON GOOGLE'
+
+  //inputUser.placeholder = 'Ingresa tu Usuario';
+  inputEmail.placeholder = 'Ingresa tu Email';
   inputPassword.placeholder = 'Ingresa tu contraseña';
+
   buttonHome.addEventListener('click', () => {
     onNavigate('/');
   });
 
-  loginDiv.append(logoDivSmall, nodoh2, labelUserName, inputUser, labelPassword, inputPassword, buttonLogin, buttonHome);
+  buttonGmail.addEventListener('click', (e) => {
+    const provider = new GoogleAuthProvider();
+    const auth = getAuth();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        // ...
+      }).catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+  })
+
+  buttonLogin.addEventListener('click', (e) => {
+    let email = document.getElementById('mailregister').value;
+    let password = document.getElementById('password').value;
+    //let username = document.getElementById('username').value;
+
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        alert('User logged');
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(errorMessage);
+      });
+
+  });
+
+  loginDiv.append(logoDivSmall, nodoh2, labelEmail, inputEmail, labelPassword, inputPassword, buttonGmail, buttonLogin, buttonHome);
   return loginDiv;
+
 };
 
