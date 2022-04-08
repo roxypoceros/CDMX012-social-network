68bc4b597b6b9acd97e74d37ec00ed4cc0bbc6d9
Lourdes Santiago <lourdes.santiago.palacios@gmail.com>
2022-04-08 10:35:19 -0500

    Mensaje de cerrar sesion listo

--- src/componets/Feed.js
+++ src/componets/Feed.js
@@ -1,12 +1,13 @@
-import { onNavigate } from "../main.js";
-import { signOut } from "https://www.gstatic.com/firebasejs/9.6.9/firebase-auth.js";
-import { auth, getPosts, onSnapshot, db, collection, publishPost, orderBy, query, deletePost, getPost, updatePost } from "../firebase.js"
+import { signOut } from 'https://www.gstatic.com/firebasejs/9.6.9/firebase-auth.js';
+import { onNavigate } from '../main.js';
+import {
+  auth, getPosts, onSnapshot, db, collection, publishPost, orderBy, query, deletePost, getPost, updatePost,
+} from '../firebase.js';
 
-let editPost = false;
-let id = '';
+const editPost = false;
+const id = '';
 
 export const Feed = () => {
-
   const logoDivSmall = document.createElement('img');
   logoDivSmall.classList.add('logoDivSmallFeed');
   logoDivSmall.src = 'https://i.imgur.com/RKPm1dL.png';
@@ -25,7 +26,7 @@ export const Feed = () => {
   iconUser.classList.add('iconUser');
   iconUser.innerHTML = '<i class="fa-solid fa-circle-user"></i>';
   const titlePost = document.createElement('h3');
-  titlePost.classList.add('titlePost')
+  titlePost.classList.add('titlePost');
   titlePost.textContent = 'HOLA,';
   userContainer.appendChild(iconUser);
   userContainer.appendChild(titlePost);
@@ -40,8 +41,8 @@ export const Feed = () => {
   const buttonPublish = document.createElement('button');
   buttonPublish.classList.add('buttonPublish');
   buttonPublish.textContent = 'Publicar';
-  postContainer.appendChild(inputPost)
-  postContainer.appendChild(buttonPublish)
+  postContainer.appendChild(inputPost);
+  postContainer.appendChild(buttonPublish);
 
   const buttonSignOut = document.createElement('button');
   buttonSignOut.textContent = 'Cerrar Sesión';
@@ -50,45 +51,44 @@ export const Feed = () => {
   const containerPosts = document.createElement('section');
   containerPosts.setAttribute('id', 'containerPosts');
 
-
   buttonSignOut.addEventListener('click', () => {
     onNavigate('/');
   });
-
 
   buttonSignOut.addEventListener('click', (e) => {
     signOut(auth)
       .then(() => {
         // Signed out successful
-        alert('Cerraste sesión');
+        swal({
+          text: 'Cerraste Sesión',
+          button: 'Cerrar',
+        });
         // ...
       })
       .catch((error) => {
         alert(errorMessage);
       });
-
   });
 
   buttonPublish.addEventListener('click', (e) => {
-    const posting = inputPost
+    const posting = inputPost;
     publishPost(posting.value)
       .then(() => {
-        inputPost.value = ''
+        inputPost.value = '';
         // console.log('se publicó')
       })
       .catch(() => {
         // console.error('no se publicó')
-      })
+      });
     // aquí se deberían de obtener los datos de firestore para mandarle el contenido del post a su respectivo usuario
   });
 
-
   // const querySnapshot = await getPosts()
-  const q = query(collection(db, 'posts'), orderBy("datecreate", "desc"));
+  const q = query(collection(db, 'posts'), orderBy('datecreate', 'desc'));
   onSnapshot(q, (querySnapshot) => {
-    let html = ''
-    querySnapshot.forEach(doc => {
-      const posts = doc.data()
+    let html = '';
+    querySnapshot.forEach((doc) => {
+      const posts = doc.data();
       html += `
             <div class = "containerPosts">
               <h4>Publicado por: ${posts.email}</h4>
@@ -98,41 +98,36 @@ export const Feed = () => {
               <button class="btnEdit" data-id='${doc.id}'><i class="fa-solid fa-pencil"></i></i></button>
               </section>
             </div>
-          `
+          `;
       // console.log(doc.data())
     });
-    containerPosts.innerHTML = html
-      
-    const buttonsDelete = containerPosts.querySelectorAll('.btnDelete')
-    buttonsDelete.forEach(btn => {
+    containerPosts.innerHTML = html;
+
+    const buttonsDelete = containerPosts.querySelectorAll('.btnDelete');
+    buttonsDelete.forEach((btn) => {
       btn.addEventListener('click', function (dataset) {
         const id = this.getAttribute('data-id');
         deletePost(id);
-      })
+      });
     });
-      
-    const buttonsEdit = containerPosts.querySelectorAll('.btnEdit')
-    buttonsEdit.forEach(btn => {
+
+    const buttonsEdit = containerPosts.querySelectorAll('.btnEdit');
+    buttonsEdit.forEach((btn) => {
       // eslint-disable-next-line indent
         btn.addEventListener('click', async function (dataset) {
         const id = this.getAttribute('data-id');
         const snapPost = await getPost(id);
-        const post = snapPost.data()
-        inputPost.value = post.text
+        const post = snapPost.data();
+        inputPost.value = post.text;
         /* if (editPost = true) {
             console.log(inputPost.value = post.text)
           } else {
             console.log(updatePost())
           } */
-        buttonPublish.innerText = 'Actualizar'
-          
-        
-      })
+        buttonPublish.innerText = 'Actualizar';
+      });
     });
-
-  })
-
-
+  });
 
   // hacer una funcion que tome los parámetros del datecreate y ordenar los posts de manera descendente
 
@@ -146,8 +141,6 @@ export const Feed = () => {
           });
         } */
 
-
   feedDiv.append(logoDivSmall, buttonSignOut, userContainer, postContainer, containerPosts);
   return feedDiv;
-
 };
