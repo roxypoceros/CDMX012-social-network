b1370acd69a9d611b49d073f1450e1a8d5ed86b1
roxanaggr <roxanaggr@gmail.com>
2022-04-08 20:47:22 -0500

    se muestra el nombre y foto de google al loguearse con gmail

--- src/componets/Home.js
+++ src/componets/Home.js
@@ -13,7 +13,6 @@ export const Home = () => {
   const buttonLogIn = document.createElement('button');
   buttonLogIn.classList.add('buttonLogIn');
   buttonLogIn.textContent = 'Inicia Sesión';
-  buttonLogIn.textContent = 'Iniciar Sesión';
   buttonLogIn.addEventListener('click', () => {
     onNavigate ('/login');
   });
